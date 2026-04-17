5b6bcd21b677915d06c0200e20489b331e4e8d71
Ayano <90670998+ayyyyano@users.noreply.github.com>
2026-04-18 00:11:32 +0800

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -13,7 +13,7 @@
     <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;级</span>
   </div>
   <div class="weather" v-else>
-    <span>天气数据获取失败</span>
+    <span>君の神様になりたい</span>
   </div>
 </template>
 
@@ -91,7 +91,7 @@ const getWeatherData = async () => {
     }
   } catch (error) {
     console.error("天气信息获取失败:" + error);
-    onError("天气信息获取失败");
+    onError("网站建设中 敬请期待");
   }
 };
 

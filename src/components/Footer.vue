52cae778a6700412ab2f71d8fd637e0cbaad1155
Ayano <90670998+ayyyyano@users.noreply.github.com>
2026-04-29 21:15:01 +0800

    Update Footer.vue to include site Icp link formatting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
         <!-- 站点备案 -->
         <span>
           &nbsp;
-          <a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20262514" target="_blank">
+          <a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20262514" target="_blank">|&nbsp;
             {{ siteIcp }}
           </a>
         </span>
